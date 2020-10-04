50a8f08f689a158d9599c9aa9a649d30303a109c
SayujSujeev <sayujsujeev550@gmail.com>
2020-10-04 21:11:31 +0530

    Stat of Project....

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,7 +10,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Helloo Welcomeee ',style: TextStyle(
+        child: Text('Helloo Welcomeee........... ',style: TextStyle(
           fontSize: 30.0,
           fontWeight: FontWeight.bold,
         ),),
